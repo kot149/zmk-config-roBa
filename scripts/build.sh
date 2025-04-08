94951ccb19c1419dd9a6c6813ae466b62f7020f1
kot149 <131101078+kot149@users.noreply.github.com>
2025-04-08 22:45:24 +0900

    Add shield rgbled_adapter to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -66,8 +66,8 @@ EXTRA_MODULES_STR_RIGHT=$(IFS=';'; echo "\"${EXTRA_MODULES_RIGHT[*]}\"")
 # ビルドコマンドの設定
 if [ "$FULL_BUILD" = true ]; then
     # フルビルド用のコマンド
-    RIGHT_BUILD_CMD="west build -p -s app -d build/right -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DZMK_CONFIG=/workspaces/zmk-config/config -DSHIELD=roBa_R -DZMK_EXTRA_MODULES=$EXTRA_MODULES_STR_RIGHT"
-    LEFT_BUILD_CMD="west build -p -s app -d build/left  -b seeeduino_xiao_ble -- -DZMK_CONFIG=/workspaces/zmk-config/config -DSHIELD=roBa_L -DZMK_EXTRA_MODULES=$EXTRA_MODULES_STR_LEFT"
+    RIGHT_BUILD_CMD="west build -p -s app -d build/right -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DZMK_CONFIG=/workspaces/zmk-config/config -DSHIELD=\"roBa_R rgbled_adapter\" -DZMK_EXTRA_MODULES=$EXTRA_MODULES_STR_RIGHT"
+    LEFT_BUILD_CMD="west build -p -s app -d build/left  -b seeeduino_xiao_ble -- -DZMK_CONFIG=/workspaces/zmk-config/config -DSHIELD=\"roBa_L rgbled_adapter\" -DZMK_EXTRA_MODULES=$EXTRA_MODULES_STR_LEFT"
     RESET_BUILD_CMD="west build -p -s app -d build/reset -b seeeduino_xiao_ble -- -DZMK_CONFIG=/workspaces/zmk-config/config -DSHIELD=settings_reset"
     info_msg "Performing full build..."
 else
