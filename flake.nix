5372b4368f4e0311940747c4728e03eb6fe989f5
kot149 <131101078+kot149@users.noreply.github.com>
2025-05-18 13:54:29 +0900

    Init zmk-nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zmk-nix }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+  in {
+    packages = forAllSystems (system: rec {
+      default = firmware;
+
+      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+        name = "roBa";
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
+
+        board = "seeeduino_xiao_ble";
+        shield = "roBa_%PART% rgbled_adapter";
+        parts = [ "R" "L" ];
+        # parts = [ "R" ];
+        # parts = [ "L" ];
+        extraCmakeFlags = [ "-DCONFIG_NEWLIB_LIBC=y" ];
+
+        zephyrDepsHash = "sha256-r2BxlXWhhE8cdU/W1qdxUhPdYZtb1dTmun0smwaNdwM=";
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
+}
